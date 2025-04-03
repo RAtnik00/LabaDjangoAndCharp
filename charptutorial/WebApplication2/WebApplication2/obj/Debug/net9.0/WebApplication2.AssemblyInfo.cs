1a3cd94140e5e3a776e7c788b4cc0df060c5e6bb
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-04-03 08:33:43 +0200

    no message

--- charptutorial/WebApplication2/WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ charptutorial/WebApplication2/WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca93b6a399f23444a9478cc142abed1b1ecfb03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a484e9e9f71aac921fb11f00d898b395ada2b9bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
