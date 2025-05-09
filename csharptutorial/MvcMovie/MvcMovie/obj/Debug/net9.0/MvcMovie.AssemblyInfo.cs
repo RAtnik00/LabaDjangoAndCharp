536740cc6af378a276332a5c504728e381b0f880
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-05-09 09:05:37 +0200

    add class

--- csharptutorial/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ csharptutorial/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61bad6dba1cf5d45469ac731e8ac9cd7a163bdb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5909c653b46f38aff5a11bcdd219400404a443")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
