83150cb9d70f584093f89d70e51e63559276e646
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-05-09 09:24:16 +0200

    edited contoller

--- csharptutorial/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ csharptutorial/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53281dcde6a5bd6f6b80a2e567818c82ac8bdbfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a5b9a0b79a2523cee751cfb9e3ea13ecf467a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
