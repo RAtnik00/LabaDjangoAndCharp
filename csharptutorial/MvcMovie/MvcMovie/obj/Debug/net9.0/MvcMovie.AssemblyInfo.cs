53281dcde6a5bd6f6b80a2e567818c82ac8bdbfe
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-05-09 09:16:21 +0200

    add view

--- csharptutorial/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ csharptutorial/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536740cc6af378a276332a5c504728e381b0f880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041cef533463da07dd6952bb1e40467a9f497403")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
